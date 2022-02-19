e9d702104753082ca0537922718c0164b354cf31
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2022-02-19 14:40:46 +0400

    Add project files.

---
+++ SingletonFabrica/Factory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonFabrica
+{
+    public class Factory
+    {
+        public List<ICloning> _clones;
+
+        public Factory(List<ICloning> clones)
+        {
+            _clones = clones;
+        }
+
+        public ICloning GetClone(int num)
+        {
+            switch (num)
+            {
+                case 1: return Clone.GetClone(); break;
+                case 2: return Clone.GetClone(); break;
+                case 3: return Clone.GetClone(); break;
+                case 4: return Clone.GetClone(); break;
+            }
+            return null;
+        }
+    }
+}
